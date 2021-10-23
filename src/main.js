e05560eed2bc8a6ec6cbc3abfa3a377de0a0dfe3
behail <angbehailu@hmail.com>
2021-10-23 21:53:56 +0300

    PWA added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import "./index.css";
 import router from "./router";
 import store from "./store/store";
+import './registerServiceWorker'
 
 const app = createApp(App);
 
